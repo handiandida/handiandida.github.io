7a1cd0f49178f1fbaff6bd682e7a9d06670555df
Dida Handian <dida.handian@wgs-id.com>
2022-12-27 19:31:15 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
-        <Portfolio />
+        {/* <Portfolio /> */}
         <Testimonials />
         <Contact />
       </main>
